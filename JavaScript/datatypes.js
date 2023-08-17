2307ee7e6825134b7d5ce949dfcec14ff5880d4e
aastha-cse <guptaaastha824@gmail.com>
2023-08-17 15:47:56 +0530

    practiced datatypes

---
+++ JavaScript/datatypes.js
@@ -0,0 +1,13 @@
+let a=2;
+let b=3;
+console.log("type of a= ",typeof(a));
+console.log("type of b= ",typeof(b));
+console.log(a+b);
+
+let x='2.3';
+let y=3;
+console.log("type of x= ",typeof(x));
+console.log("type of y= ",typeof(y));
+console.log(x+y);
+console.log(parseInt(x)+y);
+console.log(parseFloat(x)+y);
