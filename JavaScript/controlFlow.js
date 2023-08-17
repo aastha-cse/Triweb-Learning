803bbba3d242f031ecfc150c2c1ec45888f78da7
aastha-cse <guptaaastha824@gmail.com>
2023-08-17 17:41:10 +0530

    practiced control flow statements

---
+++ JavaScript/controlFlow.js
@@ -0,0 +1,29 @@
+let x=3;
+let y=4;
+if(x>y) console.log("x is greater");
+else console.log("y is greater");
+
+//switch
+let a=6;
+let b=3;
+let c=2;
+switch(c)
+{
+    case 0:
+        console.log(a+b);
+        break;
+    case 1:
+        console.log(a-b);
+        break;  
+    case 2:
+        console.log(a*b);
+        break;
+    case 3:
+        console.log(a/b); 
+        break;
+    default:
+        console.log("enter number between 0-3"); 
+        break;
+}
+
+
--- JavaScript/datatypes.js
+++ JavaScript/datatypes.js
@@ -10,4 +10,28 @@ console.log("type of x= ",typeof(x));
 console.log("type of y= ",typeof(y));
 console.log(x+y);
 console.log(parseInt(x)+y);
+console.log(+x+y);
 console.log(parseFloat(x)+y);
+
+//conversions
+let z='110';
+console.log(parseInt(z,2));
+
+//literals
+console.log('binary',0b0011);
+console.log('octal',0o77);
+console.log('decimal',117);
+
+let obj={name:'Aastha',id:'34'};
+console.log(obj);
+console.log(obj.name);
+
+let object={'':'Aastha','!':'345'};
+console.log(object);
+console.log(object['']);
+console.log(object['!']);
+
+let s1='Web Developer';
+let name='Aastha';
+let s2=`My name is ${name}. I'm a ${s1}.`;
+console.log(s2);
