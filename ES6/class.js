23e13b5074f3cafe5c68b076d7789878e3f015ed
aastha-cse <guptaaastha824@gmail.com>
2023-08-24 15:22:01 +0530

    practiced class in es6

---
+++ ES6/class.js
@@ -0,0 +1,11 @@
+class student{
+    constructor(sname,classId){
+        this.sname=sname;
+        this.classId=classId;
+    }
+}
+
+const student1= new student("Aastha",12);
+const student2= new student("Agrim",9);
+console.log(student1);
+console.log(student2);
