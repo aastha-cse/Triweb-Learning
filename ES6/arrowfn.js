cae61bee8297a54854daf74145ea9d549524eeab
aastha-cse <guptaaastha824@gmail.com>
2023-08-24 12:12:07 +0530

    practiced concepts of arrow function

---
+++ ES6/arrowfn.js
@@ -0,0 +1,21 @@
+const xyz = ()=> {
+    console.log('xyz',this);
+}
+
+console.log('outer',this);
+xyz();
+
+
+//2 expression bodies
+let arr=[1,2,3,4];
+const mp=arr.map(e=>e*2);
+console.log(mp);
+
+
+const retObj =(uname, age)=>({uname:uname, age:age})
+console.log(retObj('aastha',20));
+
+const sum = (num1,num2)=>(num1+num2);
+console.log(sum(3,4));
+
+arr.forEach((e)=> console.log(e,' '));
