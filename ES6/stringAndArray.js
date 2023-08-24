2a86704d5775626301b8206b587b52d38c17a6a4
aastha-cse <guptaaastha824@gmail.com>
2023-08-24 17:29:44 +0530

    practiced string and array methods

---
+++ ES6/stringAndArray.js
@@ -0,0 +1,36 @@
+let str='I am very happy today';
+console.log(str.includes('very'));
+console.log(str.startsWith('i'));
+console.log(str.endsWith('y'));
+
+
+let s="hi guys";
+let arr= Array.from(s);
+console.log(arr);
+
+function check(){
+    console.log(arguments);
+    let a= Array.from(arguments);
+    console.log(a);
+}
+check(1,2,3,4,5);
+
+let b=['a','b','c','d'];
+let keys=b.keys();
+console.log(keys);
+
+for(let e of keys){
+   console.log(b[e]);
+}
+
+let arr1=[1,2,3,4,5];
+let firstOdd=arr1.find(getOdd);
+let idx=arr1.findIndex((value,i,ar)=>{
+    return value%2!=0;
+})
+
+function getOdd(val,i,ar){
+    return val%2==0;
+}
+console.log(firstOdd);
+console.log(idx);
