faa0cd4adf50d028af543c27b21a734436721689
aastha-cse <guptaaastha824@gmail.com>
2023-08-24 12:43:06 +0530

    understood spread operator

---
+++ ES6/spread.js
@@ -0,0 +1,18 @@
+let arr=[1,2,3,4,5];
+let arr1=[6,7];
+
+let arr2=[...arr,...arr1];
+console.log(arr);
+console.log(arr1);
+console.log(arr2);
+
+console.log("-------------");
+
+let obj={name:'Aastha' ,age:20};
+let obj1={...obj,id:1};
+console.log("obj",obj);
+console.log("obj1",obj1);
+
+obj1.name='Agrim';
+console.log('obj after',obj);
+console.log('obj1 after',obj1);
