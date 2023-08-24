7390eaae5ed46cc0b8146aca401c712a1b16f4ea
aastha-cse <guptaaastha824@gmail.com>
2023-08-24 17:43:10 +0530

    practiced some math methods

---
+++ ES6/math.js
@@ -0,0 +1,42 @@
+console.log(Math.trunc(5));
+console.log(Math.trunc(5.7));
+console.log(Math.trunc(5.1));
+console.log(Math.trunc(-5.9));
+
+console.log('----------');
+
+console.log(Math.sign(5));
+console.log(Math.sign(-5));
+console.log(Math.sign(0));
+
+console.log('----------');
+
+console.log(Math.cbrt(64));
+console.log(Math.cbrt(125));
+
+console.log('----------');
+
+console.log(Math.log2(1));
+console.log(Math.log10(10));
+
+console.log('----------');
+
+let x=Number.EPSILON;
+console.log(x);
+
+console.log('----------');
+
+let y=Number.MAX_SAFE_INTEGER;
+console.log(y);
+
+console.log('----------');
+
+let z=Number.MIN_SAFE_INTEGER;
+console.log(z);
+
+console.log('----------');
+
+let num1=12;
+let num2=4890385894730964354345242;
+console.log(Number.isInteger(num1));
+console.log(Number.isSafeInteger(num2));
