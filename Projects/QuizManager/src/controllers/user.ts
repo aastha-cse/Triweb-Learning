afbd59e15225b46d5b6e201b541bb85c8ca5d7bd
aastha-cse <guptaaastha824@gmail.com>
2023-09-05 19:36:39 +0530

    Added Schema and Interface

--- Projects/QuizManager/src/controllers/user.ts
+++ Projects/QuizManager/src/controllers/user.ts
@@ -1,8 +1,31 @@
 import { Request, Response } from "express";
+import User from '../models/user';
 
-const registerUser=(req:Request,res:Response)=>{
-    console.log(req.body);
-    res.send("Done")
+interface ReturnResponse{
+    status:"success" | "error",
+    message:String,
+    data:{}
+}
+
+const registerUser=async (req:Request,res:Response)=>{
+    let resp:ReturnResponse;
+    try {
+        const user=new User(req.body);
+        const result=await user.save();
+        if(!result){
+            resp={status:"error",message:"No result found",data:{}};
+            res.send(resp);
+        }
+        else{
+            resp={status:"success",message:"Registration Done",data:{userId:result._id}};
+            res.send(resp);
+        }
+        
+    } catch (error) {
+        //console.log(error);
+        resp={status:"error",message:"Something went wrong",data:{}};
+        res.status(500).send(resp);
+    }
 }
 
 export {registerUser};
