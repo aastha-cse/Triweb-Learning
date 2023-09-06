8ab053c915c023353fecc36c99b5c69d38076492
aastha-cse <guptaaastha824@gmail.com>
2023-09-06 15:52:13 +0530

    Added functionality to get the getails of user

--- Projects/QuizManager/src/routes/user.ts
+++ Projects/QuizManager/src/routes/user.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { registerUser } from '../controllers/user';
+import { registerUser, getUser } from '../controllers/user';
 
 const router=express.Router();
 
 router.post('/',registerUser);
+
+router.get('/:userId',getUser);
 
 export default router;
