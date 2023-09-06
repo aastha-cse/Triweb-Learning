1e46d0dded1f0bd15aece06799df6eb1c232c909
aastha-cse <guptaaastha824@gmail.com>
2023-09-06 16:07:23 +0530

    added functionality to update user data

--- Projects/QuizManager/src/routes/user.ts
+++ Projects/QuizManager/src/routes/user.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import { registerUser, getUser } from '../controllers/user';
+import { registerUser, getUser, updateUser } from '../controllers/user';
 
 const router=express.Router();
 
 router.post('/',registerUser);
 
 router.get('/:userId',getUser);
+
+router.put('/',updateUser);
 
 export default router;
