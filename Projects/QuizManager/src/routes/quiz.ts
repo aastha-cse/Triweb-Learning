6ff05221458040ffb63432853ce5d4a7c8aba9a3
aastha-cse <guptaaastha824@gmail.com>
2023-09-09 15:50:29 +0530

    quiz controller setup done

--- Projects/QuizManager/src/routes/quiz.ts
+++ Projects/QuizManager/src/routes/quiz.ts
@@ -1,24 +1,17 @@
 import express from "express";
+import {createQuiz, getQuiz, updateQuiz, deleteQuiz, publishQuiz} from '../controllers/quiz';
+import { isAuthenticated } from "../middlewares/isAuth";
+
 const router=express.Router();
 
-router.post("/",(req,res)=>{
-    res.send("created");
-});
+router.post("/", isAuthenticated, createQuiz);
 
-router.get("/:quizId",(req,res)=>{
-    res.send("get quiz");
-})
+router.get("/:quizId", isAuthenticated, getQuiz);
 
-router.put("/",(req,res)=>{
-    res.send("updated");
-})
+router.put("/", isAuthenticated, updateQuiz);
 
-router.delete("/:quizId",(req,res)=>{
-    res.send("deleted");
-})
+router.delete("/:quizId", isAuthenticated, deleteQuiz);
 
-router.patch("/patch",(req,res)=>{
-    res.send("published");
-})
+router.patch("/patch", isAuthenticated , publishQuiz);
 
 export default router;
