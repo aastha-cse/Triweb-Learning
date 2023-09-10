fc1eb97190de559de0d085abe87a88dc5ff1ce77
aastha-cse <guptaaastha824@gmail.com>
2023-09-10 18:15:35 +0530

    added functionality to submit exam function

--- Projects/QuizManager/src/controllers/exam.ts
+++ Projects/QuizManager/src/controllers/exam.ts
@@ -1,6 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import Quiz from '../models/quiz';
 import ProjectError from "../helper/error";
+import Result from "../models/result";
+
+interface ReturnResponse{
+    status:"success" | "error",
+    message:String,
+    data:{} | []
+}
 
 const startExam=async (req:Request, res:Response, next:NextFunction)=>{
     try {
@@ -16,19 +23,41 @@ const startExam=async (req:Request, res:Response, next:NextFunction)=>{
             err.statusCode=405;
             throw err;
         }
-        res.send(quiz);
+        const resp:ReturnResponse={status:"success", message:"Quiz Started", data:{quiz}};
+        res.status(200).send(resp);
     } catch (error) {
         next(error);
     }
 }
 
 const submitExam=async (req:Request, res:Response, next:NextFunction)=>{
-    const quizId=req.body.quizId;
-    const attempted_questions=req.body.attempted_questions;
-    const quiz=await Quiz.findById(quizId, {answers:1});
-    if(quiz){
+    try {
+        const quizId=req.body.quizId;
+        const attempted_questions=req.body.attempted_questions;
+        const quiz=await Quiz.findById(quizId, {answers:1});
+        if(!quiz){
+            const err=new ProjectError("Quiz not found");
+            err.statusCode=404;
+            throw err;
+        }
+        const userId=req.userId;
         const answers=quiz.answers;
-        res.send(answers);
+        const allQuestions=Object.keys(answers);
+        const total=allQuestions.length;
+        let score=0;
+        for(let i=0;i<total;i++){
+            let question_number=allQuestions[i];
+            if(!!attempted_questions[question_number] && answers[question_number]==attempted_questions[question_number]){
+                score=score+1;
+            }
+        }
+        
+        const result=new Result({userId, quizId, score, total});
+        const data=await result.save();
+        const resp:ReturnResponse={status:"success", message:"Quiz Submitted", data:{total, score, resultId:data._id}};
+        res.status(200).send(resp);
+    } catch (error) {
+        next(error);
     }
 }
 
