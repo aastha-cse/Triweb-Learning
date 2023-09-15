66ed2c0ee4ccd787350d637c084d02924866148a
aastha-cse <guptaaastha824@gmail.com>
2023-09-15 16:43:46 +0530

    Added css to product

--- React/my-app/src/components/product/ProductItem.js
+++ React/my-app/src/components/product/ProductItem.js
@@ -1,15 +1,17 @@
+import Styles from './ProductItem.module.css';
+
 function ProductItem(props){
     return <li key={props.item.id}>
         <div>
-            <div>
+            <div className={Styles.image}>
                 <img src={props.item.image} alt="Nothig to display"></img>
             </div>
-            <div>
+            <div  className={Styles.content}>
                 <h2>{props.item.product_name}</h2>
                 <p>{props.item.description}</p>
                 <h4>{props.item.price}</h4>
             </div>
-            <div>
+            <div className={Styles.btn}>
                 <button>Purchase</button>
             </div>
         </div>
--- React/my-app/src/components/product/ProductList.js
+++ React/my-app/src/components/product/ProductList.js
@@ -1,7 +1,8 @@
 import ProductItem from "./ProductItem";
+import Styles from './ProductList.module.css';
 
 function ProductList(props){
-    return <div>
+    return <div className={Styles.list}>
         {props.products.map((item)=>{
             return <ProductItem item={item}/>
         })}
