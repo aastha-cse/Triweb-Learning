45ee91fc8fdc7a26167e60d73bad93096df964fb
aastha-cse <guptaaastha824@gmail.com>
2023-09-18 18:21:17 +0530

    added wrapper to product form

--- React/my-app/src/components/product/AddProductForm.js
+++ React/my-app/src/components/product/AddProductForm.js
@@ -1,29 +1,30 @@
 import Styles from './AddProductForm.module.css';
+import CustomWrapper from '../layouts/Wrapper.js';
 
 function AddProductForm(){
-    return <div>
+    return <CustomWrapper>
         <form className={Styles.form}>
             <div className={Styles.groups}>
-                <label>Enter Product Name:</label>
+                <label htmlFor="product_name">Enter Product Name:</label>
                 <input type="text" placeholder="product_name"/>
             </div>
             <div className={Styles.groups}>
-                <label>Enter Product Image:</label>
+                <label htmlFor="image">Enter Product Image:</label>
                 <input type="text" placeholder="image"/>
             </div>
             <div className={Styles.groups}>
-                <label>Enter Product Price:</label>
+                <label htmlFor="price">Enter Product Price:</label>
                 <input type="text" placeholder="price"/>
             </div>
             <div className={Styles.groups}>
-                <label>Enter Product Description:</label>
+                <label htmlFor="description">Enter Product Description:</label>
                 <textarea rows="5" id="description"/>
             </div>
             <div className={Styles.btn}>
                 <button>Submit</button>
             </div>
         </form>
-    </div>
+    </CustomWrapper>
 }
 
 export default AddProductForm;
