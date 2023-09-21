02e79700bc088d6d278255bc1a77e86926958e1c
aastha-cse <guptaaastha824@gmail.com>
2023-09-21 16:30:42 +0530

    Learned about navigation hook, redirect after form submit

--- React/my-app/src/pages/AddProduct.js
+++ React/my-app/src/pages/AddProduct.js
@@ -1,15 +1,17 @@
+import {useNavigate} from "react-router-dom";
 import AddProductForm from "../components/product/AddProductForm";
 
 function AddProduct(){
+    const navigate=useNavigate();
     function addProductHandler(newProduct){
-        console.log("newProd",newProduct);
         fetch("http://localhost:3002/product",{
             method:"POST",
             body: JSON.stringify(newProduct),
             headers:{
                 'Content-Type':'application/json'
             }
-        })
+        }).then(response=>navigate("/",{replace:true}))
+        .catch(err=>console.log(err));
     }
     return <AddProductForm addProductHandler={addProductHandler}/>
 }
