76c0e477bf99d5baa70a18e6eb5e80e30c8eaf1f
aastha-cse <guptaaastha824@gmail.com>
2023-09-25 11:12:28 +0530

    Added the winner and playagain functionality

--- Projects/TicTacToe/tic-tac-toe/src/components/Board.jsx
+++ Projects/TicTacToe/tic-tac-toe/src/components/Board.jsx
@@ -6,17 +6,55 @@ const Board = () => {
   const [state, setState] = useState(Array(9).fill(null));
   const [isXTurn, setIsXTurn] = useState(true);
 
+  const checkWinner = () => {
+    const winnerLogic = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let logic of winnerLogic) {
+      const [a, b, c] = logic;
+      if (state[a] !== null && state[a] === state[b] && state[a] === state[c]) {
+        return state[a];
+      }
+    }
+
+    return false;
+  };
+
+  const isWinner = checkWinner();
+
   const handleClick = (index) => {
     if (state[index] !== null) {
       return;
     }
+
     const copyState = [...state];
     copyState[index] = isXTurn ? "X" : "O";
     setState(copyState);
     setIsXTurn(!isXTurn);
   };
+
+  const handleReset = () => {
+    setState(Array(9).fill(null));
+  };
+  
   return (
     <div className={Styles.board}>
+      {isWinner ? (
+        <>
+          {isWinner} won the game{" "}
+          <button onClick={handleReset}>Play Again</button>
+        </>
+      ) : (
+        <>
+          <h4>Player {isXTurn ? "X" : "O"} please move</h4>
       <div className={Styles.row}>
         <Square onClick={() => handleClick(0)} value={state[0]} />
         <Square onClick={() => handleClick(1)} value={state[1]} />
@@ -32,6 +70,8 @@ const Board = () => {
         <Square onClick={() => handleClick(7)} value={state[7]} />
         <Square onClick={() => handleClick(8)} value={state[8]} />
       </div>
+      </>
+      )}
     </div>
   );
 };
