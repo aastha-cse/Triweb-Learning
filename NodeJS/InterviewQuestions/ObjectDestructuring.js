1fda57bb0b8063a5b86ac3a1e3e7fb86830a51ab
aastha-cse <guptaaastha824@gmail.com>
2023-10-12 18:05:35 +0530

    practiced object destructuring

---
+++ NodeJS/InterviewQuestions/ObjectDestructuring.js
@@ -0,0 +1,19 @@
+const marksOfStundent = [40,98,89,65,34,78,56,78,99,87];
+
+[subject1Marks,subject2Marks, subject3Marks,subject4Marks,subject5Marks, ...otherMarks] = marksOfStundent;
+
+console.log("subject1Marks",subject1Marks);
+console.log("subject2Marks",subject2Marks);
+console.log("subject3Marks",subject3Marks);
+console.log("subject4Marks",subject4Marks);
+console.log("subject5Marks",subject5Marks);
+console.log("Other activity marks",otherMarks);
+
+const student = {
+    'uname': "Aastha",
+    'mobile': 2345454,
+    'address': "India"
+};
+
+const {uname, mobile} = student;
+console.log(uname," and mobile = ",mobile);
