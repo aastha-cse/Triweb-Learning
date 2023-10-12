7f3935a1ef70db05ed29b0e24a182ec61b04ffe0
aastha-cse <guptaaastha824@gmail.com>
2023-10-12 18:33:45 +0530

    difference between setimmediate() and settimeout()

---
+++ NodeJS/InterviewQuestions/settimeout.js
@@ -0,0 +1,13 @@
+function add(callby,n1,n2){
+    console.log(callby,":",n1+n2);
+}
+
+setImmediate(()=>{
+    add("SetImmediate",5,3);
+})
+
+setTimeout(() => {
+    add("SetTimeout",3,6);
+}, 0);
+
+add("Direct",1,2);
